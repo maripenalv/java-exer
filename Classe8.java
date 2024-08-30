13b73d565712b4209113db2f683f71978ec7b8ea
Mariana Penalva <marianapenalva07@gmail.com>
2024-08-30 11:10:32 -0300

    Add files via upload

---
+++ Classe8.java
@@ -0,0 +1,24 @@
+package curso;
+
+import java.util.Scanner;
+
+public class Classe8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int teste= 0b100000;
+        System.out.print("Escolha um número: ");
+        int nmr= sc.nextInt();
+        
+        if ((nmr & teste) != 0){
+        	System.out.print("O sexto bit é igual a 1(verdadeiro)");
+        }
+        
+        else {
+        	System.out.print("O sexto bit é 0(falso)");
+        }
+        
+        sc.close();
+    }
+}
